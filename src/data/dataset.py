5bf0e08693cfbcc2f371a5526b85cd9d9d9f3273
ekulikov <ekulikov@quantumbrains.com>
2023-03-31 13:39:01 +0000

    'add new dataset type'

--- src/data/dataset.py
+++ src/data/dataset.py
@@ -1,6 +1,6 @@
 import torch
 from torch.utils.data import Dataset
-
+import pandas as pd
 
 def create_synth_data(n_samples,seq_len=100,n_features=50,alpha=200,betta=3):
     t = torch.linspace(0,1,steps=seq_len)
@@ -18,3 +18,57 @@ class SynthDataset(Dataset):
     def __getitem__(self, idx):
 
         return self.data[idx]
+
+
+class SimpleParquetDataset(Dataset):
+    def __init__(self, path,seq_len=100):
+        self.seq_len = seq_len
+        self.data = pd.read_parquet(path)
+
+    def __len__(self):
+        return len(self.data) - self.seq_len
+
+    def __getitem__(self, idx):
+
+        X = self.data.iloc[idx : idx + self.seq_len]
+        X = (X - X.mean()) /( X.std() + 1)
+
+        return torch.from_numpy(X.values).float().T
+    
+
+def choose_fold(folds,idx):
+    for i,f in enumerate(folds):
+        if idx < f:
+            return i
+
+class HourParquetDataset(Dataset):
+    def __init__(self, paths : list,seq_len=100,stats = [],mode='train'):
+        self.seq_len = seq_len
+
+        #read datasets
+        self.dsets = [pd.read_parquet(p) for p in paths]
+        sizes = [len(d) - seq_len for d in self.dsets]
+        self.folds = torch.cumsum(torch.tensor(sizes),0)
+        
+        self.stats = [[d.mean(),d.std()] for d in self.dsets]
+
+        if len(stats) == 1:
+            self.stats[0] = stats[-1]
+
+        #norm datasets
+        for i in range(len(self.stats) -1):
+            self.dsets[i+1] = (self.dsets[i+1] - self.stats[i][0] ) / (self.stats[i][1] + 1 )
+
+        self.dsets[0] = (self.dsets[0] - self.stats[0][0] ) / (self.stats[0][1] + 1 )
+
+
+
+    def __len__(self):
+        return self.folds[-1]
+
+    def __getitem__(self, idx):
+
+        fold = choose_fold(self.folds,idx)
+        X = self.dsets[fold][idx:idx+self.seq_len]
+
+        return torch.from_numpy(X.values).float().T
--- train.py
+++ train.py
@@ -1,19 +1,25 @@
 from src.nn.pl_model import LitAutoEncoder
-from src.data.dataset import SynthDataset
+from src.data.dataset import HourParquetDataset
 
 from torch import optim, nn, utils, Tensor
 
 import lightning.pytorch as pl
 
 
-autoencoder = LitAutoEncoder()
+autoencoder = LitAutoEncoder(in_chans=75)
 
-dataset = SynthDataset(100000,seq_len=100,n_features=50,alpha=200,betta=3)
-eval_dataset = SynthDataset(1000,seq_len=100,n_features=50,alpha=200,betta=3)
+paths = ['/workspace/TI-MAE/2022-12-02_13:00:00.parquet',
+         '/workspace/TI-MAE/2022-12-09_13:00:00.parquet',
+         '/workspace/TI-MAE/2022-12-13_13:00:00.parquet']
 
-train_loader = utils.data.DataLoader(dataset,64)
-eval_loader = utils.data.DataLoader(eval_dataset,64)
+eval_paths = ['/workspace/TI-MAE/2022-12-14_18:00:00.parquet']
+
+train_dataset = HourParquetDataset(paths,100)
+eval_dataset = HourParquetDataset(eval_paths,100,stats=[train_dataset.stats[-1]])
+
+train_loader = utils.data.DataLoader(train_dataset,64,num_workers=16)
+eval_loader = utils.data.DataLoader(eval_dataset,64,num_workers=16)
 
 
-trainer = pl.Trainer(max_epochs=10,val_check_interval = 100)
+trainer = pl.Trainer(max_epochs=10,val_check_interval = 5000)
 trainer.fit(autoencoder,train_loader,eval_loader)
